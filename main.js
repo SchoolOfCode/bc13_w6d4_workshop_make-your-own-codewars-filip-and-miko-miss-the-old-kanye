65442d8c4694836f5d8e29cad5436ceb98134b12
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-03 14:25:03 +0000

    Finall touch

--- main.js
+++ main.js
@@ -3,11 +3,12 @@ Write a function that takes a number and returns that number in descending order
 descendingOrder(123) should return (321)
 descendingOrder(89754258) should return (98875542)
 descendingOrder(4446669) should return (9666444)
+Good luck!
 */
 
 //👉 Write the function your CodeWarriors will start with below here:
 export function descendingOrder(n){
     //Your code goes here..
-    return parseInt(n.toString().split('').sort().reverse().join(""))
+
 };
 
