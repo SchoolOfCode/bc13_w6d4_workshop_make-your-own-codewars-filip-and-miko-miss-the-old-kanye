507752bd4109d418173c4741398885da951cb8ff
KETAMINION <106491264+KETAMINION@users.noreply.github.com>
2022-11-03 14:23:06 +0000

    Creating Kata for function that changes an number in descending order

--- main.test.js
+++ main.test.js
@@ -1,1 +1,33 @@
 //👉 Write your tests below here:
+import { test, expect } from '@jest/globals'
+import { descendingOrder } from "./main";
+
+test("Checks descending order for 0", () => {
+    const actual = descendingOrder(0)
+    const expected = 0
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checks descending order for 12", () => {
+    const actual = descendingOrder(12)
+    const expected = 21
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checks descending order for 89754258", () => {
+    const actual = descendingOrder(89754258)
+    const expected = 98875542
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checks descending order for 4446669", () => {
+    const actual = descendingOrder(4446669)
+    const expected = 9666444
+    expect(actual).toStrictEqual(expected)
+});
+
+test("Checks descending order for 986724156", () => {
+    const actual = descendingOrder(986724156)
+    const expected = 987665421
+    expect(actual).toStrictEqual(expected)
+});
